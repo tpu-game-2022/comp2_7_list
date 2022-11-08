a5e001d0f10f70b4ca3eed7861f1a7ff15bacaed
Nyasa0405 <105965435+Nyasa0405@users.noreply.github.com>
2022-11-08 11:02:14 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -9,6 +9,7 @@ void initialize_node(node* p, int val)
 {
 	p->pNext = NULL;
 	p->data = val;
+
 }
 
 // リストを初期化
@@ -21,12 +22,18 @@ void initialize_list(list* l)
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
+	p->pNext = l->header;
+	l->header = p;
 	// ToDo: リストにノードを追加するソースコードを実装する
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
+	if (p->pNext != NULL)
+	{
+		p->pNext = p->pNext->pNext;
+	}
 	// ToDo: リストからpで指定したノードの次のノードを削除する
 }
 
@@ -34,6 +41,7 @@ void remove_next(list* l, node* p)
 // pの次のノードを取得
 node* get_next(node* p)
 {
+
 	if (!p) return NULL;
 
 	return p->pNext;
